32dbcf5ef47e9526973fcb9e60c88298fb063d7c
Bruno Habermann <bruno.habermann@dextra-sw.com>
2020-08-31 21:50:20 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using Projeto.Testes;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var teste = new Teste(5);
+            teste.MultiplicarPor2();
+            Console.WriteLine(teste.Value());
+
+            if (teste.Value() == 10)
+            { // begin
+                Console.WriteLine("Deu certo");
+            } // end
+            else
+            {
+                Console.WriteLine("Deu errado");
+            }
+
+            var count = 0;
+            while (count < 10)
+            {
+                Console.WriteLine(count);
+                count++;
+            }
+
+            for (int i = 10; i < 0; i--)
+            {
+                Console.WriteLine(count);
+            }
+
+            // Repeat
+            var value = 0;
+            do
+            {
+                value++;
+                Console.WriteLine(value);
+            } while (value < 10);
+        }
+    }
+}
---
+++ ConsoleApp1/Teste.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Markup;
+
+namespace Projeto.Testes
+{
+    public class Teste
+    {
+        private int MyProperty { get; set; }
+        protected int Property2 { get; set; }
+
+        // Construtor sem argumentos
+        public Teste()
+        {
+            MyProperty = 0;
+        }
+
+        // Construtor com argumentos
+        public Teste(int property)
+        {
+            MyProperty = property;
+        }
+
+        // Método Sem Retorno = Procedure
+        public void MultiplicarPor2()
+        {
+            MyProperty = MyProperty * 2;
+        }
+
+        // Método com retorno = Function
+        public int Value()
+        {
+            return MyProperty;
+        }
+    }
+
+    class Teste2: Teste
+    {
+        public int RetornaProperty2()
+        {
+            return Property2;
+        }
+    }
+}
